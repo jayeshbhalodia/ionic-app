12f3589c0c98c42f0657198b68c6ef96861c061b
ankitlight <ankit.lightwebs@gmail.com>
2016-03-25 14:35:08 +0530

    update NFacts issue

--- app/pages/nutritionFacts/nutritionFacts.ts
+++ app/pages/nutritionFacts/nutritionFacts.ts
@@ -9,7 +9,6 @@ export class NutritionFacts {
 	readyNFactSpin: boolean = false;
 	readNFact: [] = [];
 	nearByNFact: {} = {};
-
 	nav: NavController;
 	constructor(nav: NavController) {
 		this.nav = nav;
@@ -18,9 +17,10 @@ export class NutritionFacts {
 			name: 'Apple',
 			type: 'Fruite',
 			description: 'The apple tree is a deciduous tree in the rose family best known for its sweet, pomaceous fruit, the apple. It is cultivated worldwide as a fruit tree, and is the most widely grown species in the genus Malus.',
-			image: ['apple1.jpeg', 'apple2.jpeg', 'apple3.jpeg'],
+			image: ['nf1.jpeg', 'nf2.jpeg', 'nf3.jpeg'],
 			nutritionFacts: [{
 				name: 'apple',
+				image: 'nf1.jpeg',
 				amountPer: '100 grams',
 				calories: 52,
 				vitaminA: 1,
@@ -57,8 +57,93 @@ export class NutritionFacts {
 						text: '10g',
 						per: 0
 					}
+				},
+				cholesterol: {
+					text: '0mg',
+					per: 0
+				},
+				totelFat: {
+					text: '0.2mg',
+					per: 0,
+					tf_1: {
+						name: 'Saturated fat',
+						text: '0g',
+						per: 0
+					},
+					tf_2: {
+						name: 'Polyunsaturated fat',
+						text: '0.1g',
+						per: 0
+					},
+					tf_3: {
+						name: 'Monounsaturated fat',
+						text: '0g',
+						per: 0
+					}
 				}
-			}],
+			}, {
+				name: 'appleJuice_unsweetened',
+				image: 'nf2.jpeg',
+				amountPer: '100 grams',
+				calories: 46,
+				vitaminA: 0,
+				calcium: 0,
+				vitaminD: 0,
+				vitaminB12: 0,
+				vitaminC: 1,
+				iron: 0,
+				vitaminB6: 0,
+				magnesium: 1,
+				sodium: {
+					text: '4mg',
+					per: 0
+				},
+				potassium: {
+					text: '101mg',
+					per: 2
+				},
+				protein: {
+					text: '0.1g',
+					per: 0
+				},
+				total_carbohydrate: {
+					text: '11g',
+					per: 3,
+					tc_1: {
+						name: 'Dietary fiber',
+						text: '0.2g',
+						per: 0
+					},
+					tc_2: {
+						name: 'Sugar',
+						text: '10g',
+						per: 0
+					}
+				},
+				cholesterol: {
+					text: '0mg',
+					per: 0
+				},
+				totelFat: {
+					text: '0.1g',
+					per: 0,
+					tf_1: {
+						name: 'Saturated fat',
+						text: '0g',
+						per: 0
+					},
+					tf_2: {
+						name: 'Polyunsaturated fat',
+						text: '0g',
+						per: 0
+					},
+					tf_3: {
+						name: 'Monounsaturated fat',
+						text: '0g',
+						per: 0
+					}
+				}
+			}]
 		}];
 
 		setTimeout(() => {
